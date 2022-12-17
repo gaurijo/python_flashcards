dab5ca178494d59250a2bd45ca6b36fa8d05081e
Gauri Joshi <gauri728@gmail.com>
2022-12-16 21:33:22 -0800

    Card class initialized

---
+++ card.py
@@ -0,0 +1,12 @@
+# create a Card class that intializes with a question, answer, category
+class Card():
+  def __init__(self, question, answer, category):
+    self.question = question 
+    self.answer = answer 
+    self.category = category 
+
+card = Card("What is the tallest Mountain on Earth?", "Everest", "Geography")
+
+print(card.question)
+print(card.answer)
+print(card.category)
